45acf2a00fba6dc184322061a63d470df4571b55
SofiiaOlenyn <sofiia.olenyn@ukma.edu.ua>
2022-02-12 19:45:11 +0200

    Initial commit

---
+++ src/main/java/com/example/JavaEE2/model/Book.java
@@ -0,0 +1,15 @@
+package com.example.JavaEE2.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Book {
+
+    String title;
+    String isbn;
+    String author;
+}
